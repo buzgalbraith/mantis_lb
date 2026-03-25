0eb8222f4bb0bf20dd4a3c27229ebe31d4200231
buz galbraith <buzgalbraith@gmail.com>
2026-03-25 20:25:54 +0100

    Added initial sketch method and code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,77 @@
-
 mod sketch;
-use sketch::{read_sketch, write_sketch, sketch_dir_files, merge_sketches, compare_sketches};
-use clap::Parser;
+
+use clap::{Parser, Subcommand};
+use rand::random;
+use sketch::{
+    compare_sketches, make_initial_sketch, merge_sketches, read_sketch, sketch_dir_files,
+    write_sketch,
+};
 use sourmash::signature::SigsTrait;
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
-    /// Directory with FastQ files 
-    #[arg(short='d', long)]
-    fastq_dir: String,
+    #[command(subcommand)]
+    command: Command,
 }
 
+#[derive(Subcommand, Debug)]
+enum Command {
+    SketchFiles {
+        #[arg(long, default_value_t=String::from("fastq_files"), short='d')]
+        fastq_dir: String,
+        #[arg(long, default_value_t = 1000, short = 's')]
+        scaled: u32,
+        #[arg(long, default_value_t = 21, short = 'k')]
+        ksize: u32,
+    },
 
+    BuildIndex {
+        #[arg(long, default_value_t=String::from("fastq_files"), short='d')]
+        fastq_dir: String,
+        #[arg(long, default_value_t=String::from("initial_index") )]
+        sig_dir: String,
+        #[arg(long, default_value_t = 5, short = 'n')]
+        num_index: u32,
+        #[arg(long, default_value_t = 1000, short = 's')]
+        scaled: u32,
+        #[arg(long, default_value_t = 21, short = 'k')]
+        ksize: u32,
+    },
+}
 
 fn main() {
     let args = Args::parse();
-    println!("Reading from {a}\nSketching with SourMash!", a=args.fastq_dir);
-    let sketches = sketch_dir_files(&args.fastq_dir);
-    let merged = merge_sketches(&sketches);
-    let filename = "merged.sig";
-    println!("Merged sketch contains {} hashes", merged.size());
-    write_sketch(filename, &merged);
-    let read_merged = read_sketch(filename);
-    println!("Read the merged sketch result contains {} hashes", read_merged.size());
-    let res = compare_sketches(&sketches[0], &sketches[1]); 
-    println!("similarity {}", res);
+    match args.command {
+        Command::SketchFiles {
+            fastq_dir,
+            scaled,
+            ksize,
+        } => {
+            println!("Reading from {a}\nSketching with SourMash!", a = fastq_dir);
+            let seed: u64 = random();
+            let sketches = sketch_dir_files(&fastq_dir, scaled, ksize, Some(seed));
+            let merged = merge_sketches(&sketches, scaled, ksize, seed);
+            let filename = "merged.sig";
+            println!("Merged sketch contains {} hashes", merged.size());
+            write_sketch(filename, &merged);
+            let read_merged = read_sketch(filename);
+            println!(
+                "Read the merged sketch result contains {} hashes",
+                read_merged.size()
+            );
+            let res = compare_sketches(&sketches[0], &sketches[1]);
+            println!("similarity {}", res);
+        }
+        Command::BuildIndex {
+            fastq_dir,
+            sig_dir,
+            num_index,
+            scaled,
+            ksize,
+        } => {
+            println!("Building index from {fastq_dir} saving to {sig_dir}");
+            make_initial_sketch(&fastq_dir, num_index, scaled, ksize, &sig_dir);
+        }
     }
+}
--- src/sketch.rs
+++ src/sketch.rs
@@ -1,17 +1,20 @@
+use needletail::{Sequence, parse_fastx_file};
+use rand::random;
+use sourmash::encodings::HashFunctions;
 use sourmash::prelude::ToWriter;
 use sourmash::{signature::SigsTrait, sketch::minhash::KmerMinHash};
-use sourmash::encodings::HashFunctions;
-use needletail::{Sequence, parse_fastx_file};
 use std::fs;
 
-pub fn sketch_file(path: &str) -> KmerMinHash {
+// wrapper for sketching an entire fastQ file
+pub fn sketch_file(path: &str, scaled: u32, ksize: u32, seed: Option<u64>) -> KmerMinHash {
+    let seed = seed.unwrap_or(random());
     let mut mh: KmerMinHash = KmerMinHash::new(
-        1000,    // scaled size
-        20,   // k-mer size
+        scaled, // scaled size
+        ksize,  // k-mer size
         HashFunctions::Murmur64Dna,
-        42,   
+        seed,
         false, // track abundance
-        0, // if 0 use scaled
+        0,     // if 0 use scaled
     );
 
     let mut reader = parse_fastx_file(path).expect("valid path/file");
@@ -25,44 +28,101 @@ pub fn sketch_file(path: &str) -> KmerMinHash {
     mh
 }
 
-pub fn sketch_dir_files(fastq_dir:&str)->Vec<KmerMinHash>{
+// Sketch all fastQ files in a directory
+pub fn sketch_dir_files(
+    fastq_dir: &str,
+    scaled: u32,
+    ksize: u32,
+    seed: Option<u64>,
+) -> Vec<KmerMinHash> {
     let paths = fs::read_dir(fastq_dir).unwrap();
     let mut sketches: Vec<KmerMinHash> = Vec::new();
     for path in paths {
-        sketches.push( sketch_file(path.unwrap().path().to_str().expect("missing_path")));
+        let path = path.unwrap().path();
+        let ext = path.extension().and_then(|e| e.to_str());
+        if ext == Some("fastq") || ext == Some("fastq.gz") {
+            sketches.push(sketch_file(
+                path.to_str().expect("missing_path"),
+                scaled,
+                ksize,
+                seed,
+            ));
+        }
     }
-    return sketches;
+    sketches
 }
 
-pub fn merge_sketches(sketches:&Vec<KmerMinHash>)->KmerMinHash{
-    let mut merged: KmerMinHash = KmerMinHash::new(
-        1000,    // num=0 → use scaled mode
-        20,   // ksize
-        HashFunctions::Murmur64Dna,
-        42,   // seed
-        false, // track abundance
-        0, // scaled
-    );
-    for sketch in sketches{
+// merge a vector of sketches into a single representation
+pub fn merge_sketches(
+    sketches: &Vec<KmerMinHash>,
+    scaled: u32,
+    ksize: u32,
+    seed: u64,
+) -> KmerMinHash {
+    // seed must be provided and match the merge
+    let mut merged: KmerMinHash =
+        KmerMinHash::new(scaled, ksize, HashFunctions::Murmur64Dna, seed, false, 0);
+    for sketch in sketches {
         merged.merge(sketch).expect("error");
     }
-    return  merged;
+    merged
+}
+// super loose wrapper for getting sketch similarly, only here for the sake of completeness
+pub fn compare_sketches(sketch_a: &KmerMinHash, sketch_b: &KmerMinHash) -> f64 {
+    sketch_a.similarity(sketch_b, false, false).expect("error")
 }
 
-pub fn compare_sketches(sketch_a:&KmerMinHash, sketch_b:&KmerMinHash)->f64{
-    return sketch_a.similarity(sketch_b, false, false).expect("error");
-}
-
-
-pub fn write_sketch(path:&str, sketch:&KmerMinHash){
-    let file = fs::File::create(path).unwrap();  // create, not open
+// write a sketch to a sig file
+pub fn write_sketch(path: &str, sketch: &KmerMinHash) {
+    let file = fs::File::create(path).unwrap(); // create, not open
     let mut writer = std::io::BufWriter::new(file);
-    sketch.to_writer(& mut writer).expect("error");
-    
+    sketch.to_writer(&mut writer).expect("error");
 }
-pub fn read_sketch(path:&str)->KmerMinHash{
+// read a sketch from a sig file
+pub fn read_sketch(path: &str) -> KmerMinHash {
     let file = fs::File::open(path).unwrap();
     let reader = std::io::BufReader::new(file);
-    let mh = KmerMinHash::from_reader(reader).expect("missing");
-    return  mh;
+    KmerMinHash::from_reader(reader).expect("missing")
+}
+
+// make n initial sketches, just using a round-robin for load balancing
+pub fn make_initial_sketch(
+    fastq_dir: &str,
+    n: u32,
+    scaled: u32,
+    ksize: u32,
+    sig_dir: &str,
+) -> Vec<(u64, KmerMinHash)> {
+    let mut sketches: Vec<(u64, KmerMinHash)> = Vec::new();
+    for _ in 0..n {
+        let seed: u64 = random();
+        sketches.push((
+            seed,
+            KmerMinHash::new(scaled, ksize, HashFunctions::Murmur64Dna, seed, false, 0),
+        ));
+    }
+    let paths = fs::read_dir(fastq_dir).unwrap();
+    for (i, path) in paths.enumerate() {
+        let idx: usize = i % n as usize;
+        let path = path.unwrap().path();
+        let ext = path.extension().and_then(|e| e.to_str());
+        if ext == Some("fastq") || ext == Some("fastq.gz") {
+            let file_sketch = sketch_file(
+                path.to_str().expect("missing_path"),
+                scaled,
+                ksize,
+                Some(sketches[idx].0),
+            );
+            sketches[idx].1.merge(&file_sketch).unwrap();
+        }
+    }
+    fs::create_dir_all(sig_dir).expect("could not create sig dir");
+    // write out results
+    for i in 0..n {
+        write_sketch(
+            format!("{sig_dir}/cluster_sketch_{}.sig", i).as_str(),
+            &sketches[i as usize].1,
+        );
+    }
+    sketches
 }
